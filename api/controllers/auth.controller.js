e52cfc4772b1165cabb8b2d00c98bf1b43eb16e9
HassanMunene <awanzihassan@gmail.com>
2024-06-09 14:16:15 +0300

    completed the signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const Signup = async(req, res, next) => {
 
@@ -12,20 +13,12 @@ export const Signup = async(req, res, next) => {
 		next(errorHandler(statusCode, message));
 	}
 
-    // Validate email format:
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        const statusCode = 400;
-        const message = "Invalid email format. Please enter a valid email address.";
-        next(errorHandler(statusCode, message));
-    }
-
     // check for an existing user with the same username or email
     const existingUser = await User.findOne({ $or: [{username}, {email}] });
     if (existingUser) {
         const statusCode = 400;
         const message = `User with this ${email} already exists. Please choose a different one`
-        next(errorHandler(statusCode, message))
+        return next(errorHandler(statusCode, message))
     }
 
 	try {
@@ -41,4 +34,30 @@ export const Signup = async(req, res, next) => {
 		console.log(err.message);
 		next(err);
 	}
+}
+
+export const Signin = async(req, res, next) => {
+    const {email, password} = req.body
+    if (!email || !password || !email.trim() || !password.trim()) {
+        next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        //check If user exist
+        const validUser = await User.findOne({email});
+        if (!validUser) {
+            return next(errorHandler(400, "Invalid user credentials"))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid user credentials"));
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET_KEY)
+
+        const {password: pass, ...restDetails} = validUser._doc;
+        res.status(200).cookie("access_token", token, {httpOnly: true}).json(restDetails)
+    } catch(err) {
+        console.log(err)
+        next(err);
+    }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { Signup } from "../controllers/auth.controller.js";
+import { Signup, Signin } from "../controllers/auth.controller.js";
 
 const route = express.Router();
 
 route.post("/signup", Signup);
+route.post("/signin", Signin);
 
 export default route;
