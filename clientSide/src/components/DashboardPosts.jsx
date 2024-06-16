905cf308e505dda673ea7f51a6ac82b87f5fc85c
HassanMunene <awanzihassan@gmail.com>
2024-06-16 19:47:34 +0300

    delete post functionality completed

--- clientSide/src/components/DashboardPosts.jsx
+++ clientSide/src/components/DashboardPosts.jsx
@@ -1,12 +1,15 @@
 import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
-import {Table, Button} from "flowbite-react";
+import {Table, Button, Modal} from "flowbite-react";
 import {Link} from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi2";
 
 function DashboardPosts() {
     const {user: currentUser} = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
     const [showMore, setShowMore] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [postIdToDelete, setPostIdToDelete] = useState('');
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -42,7 +45,26 @@ function DashboardPosts() {
         } catch (error) {
             console.log(error.message);
         }
-  };
+    };
+
+    const handleDeletePost = async() => {
+        setShowModal(false);
+        try {
+            const response = await fetch(`/api/post/delete-post/${postIdToDelete}/${currentUser._id}`, {
+                method: "DELETE",
+            });
+            const responseData = await response.json();
+            if (responseData.success == true) {
+                setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete))
+            } else {
+                console.log(responseData.message);
+                return;
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    console.log(userPosts)
     return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
             {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -57,7 +79,7 @@ function DashboardPosts() {
                             <Table.HeadCell><span>Edit</span></Table.HeadCell>
                         </Table.Head>
                         {userPosts.map((post) => (
-                            <Table.Body className='divide-y'>
+                            <Table.Body className='divide-y' key={post._id}>
                                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                                     <Table.Cell>
                                         {new Date(post.updatedAt).toLocaleDateString()}
@@ -74,7 +96,8 @@ function DashboardPosts() {
                                     </Table.Cell>
                                     <Table.Cell>{post.category}</Table.Cell>
                                     <Table.Cell>
-                                        <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                                        <span onClick={() => {setShowModal(true); setPostIdToDelete(post._id)}}
+                                            className='font-medium text-red-500 hover:underline cursor-pointer'>
                                             Delete
                                         </span>
                                     </Table.Cell>
@@ -94,8 +117,22 @@ function DashboardPosts() {
                     )}
                 </>
             ) : (<p>You have no posts yet!</p>)}
+            <Modal show={showModal} onClose={() => setShowModal(false)} popup size="md">
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+                        <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                            Are you sure you want to delete this post?
+                        </h3>
+                        <div className='flex justify-center gap-4'>
+                            <Button color='failure' onClick={handleDeletePost}>Yes, Im sure</Button>
+                            <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
-
 export default DashboardPosts;
