b4ae74b837ca83eedd6055dc83b4e5ba0fb34a96
HassanMunene <awanzihassan@gmail.com>
2024-06-20 20:14:27 +0300

    complete the recent post functionality

--- clientSide/src/components/PostCard.jsx
+++ clientSide/src/components/PostCard.jsx
@@ -3,17 +3,20 @@ import {Link} from "react-router-dom";
 function PostCard ({post}) {
     return (
         <div 
-            className="relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px]">
+            className="relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[330px]">
             <Link to={`/post/${post.slug}`}>
                 <img 
                     src={post.image} 
                     alt="post-cover"
-                    className="h-[260px] w-full object-cover "/>
+                    className="h-[200px] w-full object-cover "/>
             </Link>
             <div className='p-3 flex flex-col gap-2'>
                 <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
                 <span className='italic text-sm'>{post.category}</span>
-                <Link to={`/post/${post.slug}`} className='z-10'>Read article</Link>
+                <Link to={`/post/${post.slug}`} 
+                    className='border border-teal-500 hover:bg-teal-500 hover:text-white text-center py-2 rounded-md m-2'>
+                    Read article
+                </Link>
             </div>
         </div>
     )
