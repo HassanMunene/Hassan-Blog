1e5fee88a5f4e4ff841344cd7d7b0c377d6730e4
HassanMunene <awanzihassan@gmail.com>
2024-06-23 22:06:20 +0300

    still working on project page

--- clientSide/src/pages/Projects.jsx
+++ clientSide/src/pages/Projects.jsx
@@ -1,6 +1,8 @@
 function Projects() {
 	return (
-		<div>Projects Page</div>
+		<div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      		<h1 className='text-3xl font-semibold'>Projects</h1>
+      	</div>
 	)
 }
 export default Projects;
